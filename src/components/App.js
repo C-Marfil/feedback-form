9650dd06550d2c00ef7e9bf93a108d6c0bb70973
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-07-18 09:57:36 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Stay strong and remember to have fun.</p>
+    </div>
+  );
+};
+
+export default App;
